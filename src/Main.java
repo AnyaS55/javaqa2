1eee571be3cc99042a4071608b9a93cfc7ebb108
Anya <annasedova123456@gmail.com>
2023-11-07 20:19:58 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+    public static void main (String [] args) {
+        int amount = 1000;
+        int bonus = 1;
+
+        int cashback = (amount/20 * bonus);
+
+        System.out.println(cashback);
+    }
 }
